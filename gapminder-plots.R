fad08d65064d44f5e3b4d7ae084243840ca7e449
Andrew Chang <achang313@gmail.com>
2017-08-06 10:04:33 -0700

    add gdpPercap vs lifeExp plot

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -2,5 +2,14 @@
 
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv",
               "data/gapminder-FiveYears.csv")
-
 gapminder <- read.csv("data/gapminder-FiveYears.csv")
+
+library(ggplot2)
+library(plotly)
+
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point()
+
+p <- ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point()
+ggplotly(p)
